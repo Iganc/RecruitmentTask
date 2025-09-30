93ccb419bc751c433ea51e79490e72d1a9519cba
Iganc <mrozignacy@gmail.com>
2025-09-30 12:59:01 +0200

    all tasks done

--- statements/models.py
+++ statements/models.py
@@ -1,28 +1,50 @@
 from django.db import models
-
+from django.db.models import Sum
+from django.db.models.functions import TruncMonth
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from django.db.models import Case, When, F, FloatField
 
 def report_turnover_by_year_month(period_begin, period_end):
     # TODO: TASK → make report using 1 database query without any math in python
-    # example output
-    return {
-        "2009-11": {
-            {
-                "incomes": {
-                    "PLN": 120
-                },
-                "expenses": {
-                    "PLN": 100
-                }
-            }
-        }
-    }
+    items = (
+        StatementItem.objects
+        .filter(statement__date__gte=period_begin, statement__date__lte=period_end)
+        .annotate(month=TruncMonth('statement__date'))
+        .values('month', 'currency')
+        .annotate(
+            incomes=Sum(Case(
+                When(amount__gt=0, then=F('amount')),
+                default=0,
+                output_field=FloatField()
+            )),
+            expenses=Sum(Case(
+                When(amount__lt=0, then=-F('amount')),
+                default=0,
+                output_field=FloatField()
+            )),
+        )
+        .order_by('month')
+    )
+    MainDict = {}
+    for item in items:
+        date = f"{item['month'].year}-{item['month'].month:02d}"
+        currency=str(item['currency'])
+        incomes=item['incomes']
+        expenses=item['expenses']
+        if date not in MainDict:
+            MainDict[date] = {"incomes": {}, "expenses": {}}
+        MainDict[date]["incomes"][currency] = incomes
+        MainDict[date]["expenses"][currency] = expenses
+
+    return MainDict
 
 
 class Account(models.Model):
     name = models.CharField(max_length=100)
     currency = models.CharField(max_length=3)
     # TODO: TASK → add field balance that will update automatically 
-     
+    balance = models.DecimalField(max_digits=20, decimal_places=2, default=0)
     def __str__(self):
         return f'{self.name}[{self.currency}]'
 
@@ -31,7 +53,8 @@ class Statement(models.Model):
     account = models.ForeignKey(Account, on_delete=models.PROTECT)
     date = models.DateField()
     # TODO: TASK → make sure that account and date is unique on database level
-
+    class Meta:
+        unique_together = ('account', 'date')
     def __str__(self):
         return f'{self.account} → {self.date}'
     
@@ -42,7 +65,17 @@ class StatementItem(models.Model):
     currency = models.CharField(max_length=3)
     title = models.CharField(max_length=100)
     # TODO:  TASK → add field comments (type text)
-    
+    comments = models.TextField(default="")
 
     def __str__(self):
         return f'[{self.statement}] {self.amount} {self.currency} → {self.title}'
+
+@receiver(post_save, sender=StatementItem)
+@receiver(post_delete, sender=StatementItem)
+def update_account_balance(sender, instance, **kwargs):
+    account = instance.statement.account
+    total = StatementItem.objects.filter(statement__account=account).aggregate(
+        balance=Sum('amount')
+    )['balance'] or 0
+    account.balance = total
+    account.save(update_fields=['balance'])
--- statements/tools.py
+++ statements/tools.py
@@ -2,28 +2,47 @@ import csv
 
 from django.core.exceptions import ValidationError
 from statements.models import Account, Statement, StatementItem
+from django.db import transaction
 
 def statement_import(file_handler):
     idx = 0
     # TODO: TASK → in case of errors database must not change
     # TODO: TASK → optimize database queries during import
-    for row in csv.DictReader(file_handler):
-        account = Account.objects.get_or_create(
-            name=row['account'],
-            defaults={'currency': row['currency']}
-        )[0]
-        if account.currency != row['currency']:
-            raise ValidationError('Invalid currency currency ')
+    accounts_cache = {a.name: a for a in Account.objects.all()}
+    statements_cache = {} 
+    statement_items_to_create = []
+    with transaction.atomic():
+        for row in csv.DictReader(file_handler):
+            account_name = row['account']
+            currency = row['currency']
+            date = row['date']
+            amount = row['amount']
 
-        statement = Statement.objects.get_or_create(
-            account=account,
-            date=row['date']
-        )[0]
-        StatementItem.objects.create(
-            statement=statement,
-            amount=row['amount'],
-            currency=row['currency'],
+            account = accounts_cache.get(account_name)
+            if not account:
+                account = Account(name=account_name, currency=currency)
+                accounts_cache[account_name] = account
+            if account.currency != row['currency']:
+                raise ValidationError('Invalid currency currency ')
+
+            key = (account_name, date)
+            statement = statements_cache.get(key)
+            if not statement:
+                statement = Statement(account=account, date=date)
+                statements_cache[key] = statement
+
+            statement_items_to_create.append(
+                StatementItem(statement=statement, amount=amount, currency=currency)
+            )
+            idx += 1
+
+        Account.objects.bulk_create(
+            [a for a in accounts_cache.values() if a.pk is None], batch_size=1000
         )
-        idx += 1
-    return idx
+        Statement.objects.bulk_create(
+            [s for s in statements_cache.values() if s.pk is None], batch_size=1000
+        )
+        StatementItem.objects.bulk_create(statement_items_to_create, batch_size=1000)
+
+        return idx
 
